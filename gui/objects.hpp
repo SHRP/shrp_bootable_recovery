6b38bcda5451a804d23009335367d5cd140a9212
mauronofrio <poronofrio@gmail.com>
2019-10-26 19:47:55 +0200

    Add ozip decryption for Oppo/Realme device
    This function will automatically decrypt .ozip file in a .zip and it will
    save the .zip in the same folder of the .ozip and with the same name.
    After the decryption, the flash will start.
    
    To enable this function you have to set a Boardconfig flag with the
    decryption key like i did here:
    
    https://github.com/mauronofrio/android_device_realme_RMX1901/commit/a30625f80a31b9b85e4948fc9f9beeb61d5bcb8a
    
    Original projects:
    https://github.com/sabpprook/ozip2zip
    https://github.com/bkerler/oppo_ozip_decrypt
    
    Change-Id: Ic93243a565008c442bcf2992069b2801532ac7c1

--- gui/objects.hpp
+++ gui/objects.hpp
@@ -288,8 +288,9 @@ protected:
 	ThreadType getThreadType(const Action& action);
 	void simulate_progress_bar(void);
 	int flash_zip(std::string filename, int* wipe_cache);
-    void backup_before_flash();
+	void backup_before_flash();
 	int reinject_after_flash();
+	int ozip_decrypt(std::string zip_path);
 	void operation_start(const string operation_name);
 	void operation_end(const int operation_status);
 	time_t Start;
