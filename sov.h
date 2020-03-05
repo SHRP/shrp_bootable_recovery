30989ae557c0c6029c0a4e58262fdc23bfcf3123
epicX67 <46811948+epicX67@users.noreply.github.com>
2020-03-05 20:19:41 +0530

    recovery: dipper added as official device

--- sov.h
+++ sov.h
@@ -1,1 +1,1 @@
-list<string> devices={"c103","aio_otfp","tissot","oneplus3","beryllium","mblu2","grus","rolex","RMX1851","davinci","sirius","a5y17lte","wayne","jasmine_sprout"};
+list<string> devices={"c103","aio_otfp","tissot","oneplus3","beryllium","mblu2","grus","rolex","RMX1851","davinci","sirius","a5y17lte","wayne","jasmine_sprout","dipper"};
