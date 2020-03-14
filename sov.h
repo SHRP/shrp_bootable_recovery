f1cd65842f3c9e550ad78fde41d583f3e0b0ffcf
epicX67 <46811948+epicX67@users.noreply.github.com>
2020-03-14 21:30:38 +0530

    recovery: add whyred to official devices

--- sov.h
+++ sov.h
@@ -1,1 +1,1 @@
-list<string> devices={"c103","aio_otfp","tissot","oneplus3","beryllium","mblu2","grus","rolex","RMX1851","davinci","sirius","a5y17lte","wayne","jasmine_sprout","dipper","olive","vince","violet"};
+list<string> devices={"c103","aio_otfp","tissot","oneplus3","beryllium","mblu2","grus","rolex","RMX1851","davinci","sirius","a5y17lte","wayne","jasmine_sprout","dipper","olive","vince","violet","whyred"};
